b3d9e0504099ae633cc45642567de60fad99f223
Nick Richmond <get@nickrichmond.dev>
2020-09-09 00:01:10 -0400

    Add gatsby-source-nacelle package & example

---
+++ examples/gatsby-source-nacelle/gatsby-config.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+
+module.exports = {
+  plugins: [
+    {
+      resolve: 'gatsby-source-nacelle',
+      options: {
+        nacelle_space_id: process.env.NACELLE_SPACE_ID,
+        nacelle_graphql_token: process.env.NACELLE_GRAPHQL_TOKEN
+      }
+    },
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp'
+  ]
+};

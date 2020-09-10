3aa729d06cf51c942e40428c34eb282506569c02
Nick Richmond <get@nickrichmond.dev>
2020-09-10 03:55:26 -0400

    Add /shop route PLP

---
+++ examples/gatsby-source-nacelle/src/pages/shop.js
@@ -0,0 +1,34 @@
+import * as React from 'react'
+import { Link, graphql, useStaticQuery } from 'gatsby'
+
+export default function AllProducts() {
+  const productsData = useStaticQuery(graphql`
+    query ProductsQuery {
+      allNacelleProduct {
+        edges {
+          node {
+            handle
+            title
+            featuredMedia {
+              src
+              altText
+            }
+          }
+        }
+      }
+    }
+  `)
+  return (
+    <>
+      <h1>All Products</h1>
+      <ul style={{ listStyleType: 'none', padding: '0' }}>
+        {productsData.allNacelleProduct.edges.map(product =>
+          <li key={product.node.handle}>
+            <h2><Link to={`/products/${product.node.handle}`}>{product.node.title}</Link></h2>
+            <img src={product.node.featuredMedia.src} alt={product.node.title} style={{ width: '100%' }} />
+          </li>
+        )}
+      </ul>
+    </>
+  )
+}

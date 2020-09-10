e48132017347bdcff6a11e9dfacbed2bf90f41e8
Nick Richmond <get@nickrichmond.dev>
2020-09-10 16:18:00 -0400

    Prefer wrapPageElement for layout wrapping

--- examples/gatsby/gatsby-browser.js
+++ examples/gatsby/gatsby-browser.js
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import Layout from './src/components/layout'
 
-export const wrapRootElement = ({ element }) => (
+export const wrapPageElement = ({ element }) => (
   <Layout>{element}</Layout>
 );

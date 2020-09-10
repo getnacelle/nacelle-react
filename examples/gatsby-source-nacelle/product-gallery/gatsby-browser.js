50443cec0fdb962e81df9066d8ffc0cc0cd1b25c
Nick Richmond <get@nickrichmond.dev>
2020-09-10 10:06:10 -0400

    Rearrange directory structure to make room for future examples

--- examples/gatsby-source-nacelle/product-gallery/gatsby-browser.js
+++ examples/gatsby-source-nacelle/product-gallery/gatsby-browser.js
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import Layout from './src/components/layout'
 
-export const wrapRootElement = ({ element, props }) => (
+export const wrapPageElement = ({ element, props }) => (
   <Layout {...props}>{element}</Layout>
 );

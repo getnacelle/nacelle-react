432862013a9686b223cf7a7dcc87e13c7065342e
Nick Richmond <get@nickrichmond.dev>
2020-09-10 03:56:38 -0400

    Add PDP template

---
+++ examples/gatsby-source-nacelle/src/templates/product-detail.js
@@ -0,0 +1,25 @@
+import * as React from 'react'
+import { graphql } from 'gatsby'
+
+export default function ProductDetail({ data }) {
+  const { title, featuredMedia } = data?.nacelleProduct
+  return (
+    <>
+      <h2>{title}</h2>
+      <img src={featuredMedia.src} alt={featuredMedia.altText} style={{ width: '100%' }} />
+    </>
+  )
+}
+
+export const query = graphql`
+  query ProductQuery($handle: String!) {
+    nacelleProduct(handle: {eq: $handle}) {
+      handle
+      title
+      featuredMedia {
+        src
+        altText
+      }
+    }
+  }
+`

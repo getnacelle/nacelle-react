76e27a8bffdd551421df72a7706d0ad52177ac20
Nick Richmond <get@nickrichmond.dev>
2020-09-10 03:54:36 -0400

    Add homepage

---
+++ examples/gatsby-source-nacelle/src/pages/index.js
@@ -0,0 +1,18 @@
+import * as React from 'react'
+
+export default function HomePage() {
+  return (
+    <>
+      <h2>
+        Welcome!
+      </h2>
+      <p>
+        Use the links in the navigation bar
+        to navigate to Product Loading Pages and
+        Product Detail Pages created with data
+        sourced with 'gatsby-source-nacelle'
+      </p>
+    </>
+  )
+}
+

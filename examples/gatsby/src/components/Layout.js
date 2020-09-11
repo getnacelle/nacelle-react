50c1bf51f6a52bf662ce655b28bd8acc7ba11f90
Nick Richmond <get@nickrichmond.dev>
2020-09-10 21:34:50 -0400

    Formatting

--- examples/gatsby/src/components/Layout.js
+++ examples/gatsby/src/components/Layout.js
@@ -1,5 +1,5 @@
-import * as React from 'react'
-import { Link, graphql, useStaticQuery } from 'gatsby'
+import * as React from 'react';
+import { Link, graphql, useStaticQuery } from 'gatsby';
 
 export default function Layout({ children }) {
   const linkListData = useStaticQuery(graphql`
@@ -15,16 +15,26 @@ export default function Layout({ children }) {
         }
       }
     }
-`)
-  const mainMenu = linkListData.nacelleSpace.linklists.find(linkList => linkList.handle === 'main-menu')
+  `);
+  const mainMenu = linkListData.nacelleSpace.linklists.find(
+    (linkList) => linkList.handle === 'main-menu'
+  );
   return (
     <>
-      <nav style={{ display: 'flex', justifyContent: 'space-around', marginBottom: '3em' }}>
-        {mainMenu.links.map(link => (
-          <Link to={link.to} key={link.to}>{link.title}</Link>
+      <nav
+        style={{
+          display: 'flex',
+          justifyContent: 'space-around',
+          marginBottom: '3em'
+        }}
+      >
+        {mainMenu.links.map((link) => (
+          <Link to={link.to} key={link.to}>
+            {link.title}
+          </Link>
         ))}
       </nav>
-      { children }
+      {children}
     </>
-  )
+  );
 }

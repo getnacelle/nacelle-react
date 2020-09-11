2983fd8585eef5dc479ec2077883726e7d138182
Nick Richmond <get@nickrichmond.dev>
2020-09-10 23:22:15 -0400

    Update gatsby-source-nacelle package name in example project

--- examples/gatsby/gatsby-config.js
+++ examples/gatsby/gatsby-config.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 module.exports = {
   plugins: [
     {
-      resolve: 'gatsby-source-nacelle',
+      resolve: '@nacelle/gatsby-source-nacelle',
       options: {
         nacelle_space_id: process.env.NACELLE_SPACE_ID,
         nacelle_graphql_token: process.env.NACELLE_GRAPHQL_TOKEN

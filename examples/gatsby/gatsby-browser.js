257f31dd72943643d077d72c3781581d85fe166b
Nick Richmond <get@nickrichmond.dev>
2020-11-18 11:29:27 -0500

    Fix import; Pass props to layout

--- examples/gatsby/gatsby-browser.js
+++ examples/gatsby/gatsby-browser.js
@@ -1,4 +1,6 @@
 import * as React from 'react';
-import Layout from './src/components/layout';
+import Layout from './src/components/Layout';
 
-export const wrapPageElement = ({ element }) => <Layout>{element}</Layout>;
+export const wrapPageElement = ({ element, props }) => (
+  <Layout {...props}>{element}</Layout>
+);

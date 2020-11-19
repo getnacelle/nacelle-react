297e1ecb35b01a3f3cbc3072aa78bff0123618f8
Nick Richmond <get@nickrichmond.dev>
2020-11-19 09:49:49 -0500

    Add comments for clarity & to guide code additions

--- packages/gatsby-source-nacelle/src/source-nodes/index.js
+++ packages/gatsby-source-nacelle/src/source-nodes/index.js
@@ -1,4 +1,8 @@
-// Node sourcing utilities that can be called in the exports.sourceNodes of `gatsby-node.js`
+// Node sourcing utilities that can be called in the `exports.sourceNodes` of `gatsby-node.js`.
+//
+// New utilities should be functions with the parameters: (gatsbyApi, pluginOptions, config),
+// where `gatsbyApi` and `pluginOptions` are the first two arguments of `exports.sourceNodes`
+// in `gatsby-node.js`, and `config` is an optional configuration object.
 
 exports.sourceContentfulPreviewNodes = require('./contentful-preview');
 exports.sourceNacelleNodes = require('./nacelle');

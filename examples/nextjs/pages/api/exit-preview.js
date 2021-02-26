ed87e4f2c7266d8bd51ed2694f868d7dbfa95927
Nick Richmond <5732000+NWRichmond@users.noreply.github.com>
2021-02-25 19:06:48 -0500

    handle response / redirect to homepage on preview exit

--- examples/nextjs/pages/api/exit-preview.js
+++ examples/nextjs/pages/api/exit-preview.js
@@ -2,4 +2,5 @@ export default function handler(_req, res) {
   // Clears the preview mode cookies.
   console.info('[nacelle] exiting preview mode');
   res.clearPreviewData();
+  res.redirect('/');
 }

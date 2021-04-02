61a7ebe35c39e4f30c6e4da3dcf580680efaee3f
Nick Richmond <get@nickrichmond.dev>
2021-04-02 15:10:06 -0400

    redirect securely

--- examples/nextjs/pages/api/preview.js
+++ examples/nextjs/pages/api/preview.js
@@ -1,6 +1,8 @@
+import nacelleClient from '~/services/nacelle';
+
 // To view unpublished products, collections, or content, provide the
 // desired destination via the `path` param: `/api/preview?path=/pages/first-draft`
-export default function handler(req, res) {
+export default async function handler(req, res) {
   const previewModeSecret = 'my-preview-mode-secret'; // IMPORTANT! Update this value.
   const { secret } = req.query;
 
@@ -20,17 +22,76 @@ export default function handler(req, res) {
     });
   }
 
-  const path = req.query.path || '/';
+  const { path = '/' } = req.query;
 
-  // Enable Preview Mode by setting the cookies
-  res.setPreviewData({});
-  console.info('[nacelle] preview mode enabled');
-
-  // Redirect to the path from the fetched post
-  // We don't redirect to req.query.slug as that might lead to open redirect vulnerabilities
-  if (req.query.path?.endsWith('/homepage')) {
+  if (path === '/') {
+    // Set cookies to enable Preview Mode
+    res.setPreviewData({});
+    console.info('[nacelle] preview mode enabled');
     res.redirect('/');
   }
 
-  res.redirect(path);
+  // Redirecting to req.query.path might lead to open redirect vulnerabilities.
+  // Instead, we'll use the handle from the provided path to fetch data with
+  // the Nacelle Client JS SDK. If a result is found, we'll redirect to the
+  // path associated with the data fetched with the Nacelle Client JS SDK.
+
+  async function getPathFromIndexedData({ path, locale = 'en-us', method }) {
+    const [, pathPrefix, handle] = path.split('/');
+    const data = await nacelleClient.data[method]({ handle, locale }).catch(
+      (err) => {
+        console.warn(err);
+        return null;
+      }
+    );
+    const newPath = data?.handle ? `/${pathPrefix}/${data.handle}` : null;
+    return { newPath, handle };
+  }
+
+  function handleRedirect({ res, newPath, handle, method }) {
+    // Redirect to the path from the fetched data
+    if (newPath) {
+      // Set cookies to enable Preview Mode
+      res.setPreviewData({});
+      console.info('[nacelle] preview mode enabled');
+      res.redirect(newPath);
+    } else {
+      return res.status(400).json({
+        message:
+          'Cannot enable preview mode. ' +
+          `No ${method} found with handle: '${handle}'`
+      });
+    }
+  }
+
+  if (path.startsWith('/products/')) {
+    const method = 'product';
+    const { newPath, handle } = await getPathFromIndexedData({ path, method });
+    handleRedirect({ res, newPath, handle, method });
+  }
+
+  if (path.startsWith('/collections/')) {
+    const method = 'collection';
+    const { newPath, handle } = await getPathFromIndexedData({ path, method });
+    handleRedirect({ res, newPath, handle, method });
+  }
+
+  if (path.startsWith('/pages/')) {
+    const method = 'page';
+    const { newPath, handle } = await getPathFromIndexedData({ path, method });
+    handleRedirect({ res, newPath, handle, method });
+  }
+
+  if (path.startsWith('/articles/')) {
+    const method = 'article';
+    const { newPath, handle } = await getPathFromIndexedData({ path, method });
+    handleRedirect({ res, newPath, handle, method });
+  }
+
+  if (path.startsWith('/homepage')) {
+    // Set cookies to enable Preview Mode
+    res.setPreviewData({});
+    console.info('[nacelle] preview mode enabled');
+    res.redirect('/');
+  }
 }

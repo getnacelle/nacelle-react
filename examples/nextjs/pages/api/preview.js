340c006f57e74c32a9cd0f422d4c068c7185f780
Nick Richmond <get@nickrichmond.dev>
2021-04-02 13:39:25 -0400

    check preview mode secret before enabling preview

--- examples/nextjs/pages/api/preview.js
+++ examples/nextjs/pages/api/preview.js
@@ -1,15 +1,26 @@
 // To view unpublished products, collections, or content, provide the
 // desired destination via the `path` param: `/api/preview?path=/pages/first-draft`
 export default function handler(req, res) {
-  // Check the secret and next parameters
-  // This secret should only be known to this API route and the CMS
-  if (!req.query.path) {
+  const previewModeSecret = 'my-preview-mode-secret';
+  const { secret } = req.query;
+
+  if (!secret) {
     return res.status(400).json({
       message:
-        'The path must be provided in a query param. ' +
-        'For example: https://my-preview-site/api/preview?path=/page/some-page'
+        'The preview mode secret must be provided in a query param. ' +
+        'For example: https://my-preview-site/api/preview?secret=my-preview-mode-secret'
     });
   }
+
+  if (secret !== previewModeSecret) {
+    return res.status(400).json({
+      message:
+        'Cannot enable preview mode. ' +
+        'An incorrect preview mode secret provided to the /api/preview endpoint.'
+    });
+  }
+
+  const path = req.query.path || '/';
 
   // Enable Preview Mode by setting the cookies
   res.setPreviewData({});
@@ -17,9 +28,9 @@ export default function handler(req, res) {
 
   // Redirect to the path from the fetched post
   // We don't redirect to req.query.slug as that might lead to open redirect vulnerabilities
-  if (req.query.path.endsWith('/homepage')) {
+  if (req.query.path?.endsWith('/homepage')) {
     res.redirect('/');
   }
 
-  res.redirect(req.query.path);
+  res.redirect(path);
 }

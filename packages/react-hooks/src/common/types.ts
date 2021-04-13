806ca445fc97fbe29fa8babaf708cec9ecbb4898
Levi Whalen <levi@leviwhalen.com>
2021-04-13 15:45:55 -0700

    using environment variables to set the endpoint

--- packages/react-hooks/src/common/types.ts
+++ packages/react-hooks/src/common/types.ts
@@ -3,10 +3,12 @@ import { Checkout } from '@nacelle/types';
 /**
  * @param nacelleSpaceId: the target Nacelle Space ID (string)
  * @param nacelleGraphqlToken: the GraphQL token associated with the Nacelle Space (string)
+ * @param nacelleEndpoint: the API endpoint used to send requests to Nacelle (string)
  */
 export interface Credentials {
   nacelleSpaceId: string;
   nacelleGraphqlToken: string;
+  nacelleEndpoint: string;
 }
 
 export interface CheckoutResponse {

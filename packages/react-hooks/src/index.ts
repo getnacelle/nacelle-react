6de69e897df93e5b18abc50b8a74213a2f4eaeef
Nick Richmond <get@nickrichmond.dev>
2021-05-17 15:02:18 -0400

    export all types from entry point

--- packages/react-hooks/src/index.ts
+++ packages/react-hooks/src/index.ts
@@ -2,3 +2,6 @@ import { useCart, CartProvider } from './hooks/use-cart';
 import { useCheckout, CheckoutProvider } from './hooks/use-checkout';
 
 export { useCart, CartProvider, useCheckout, CheckoutProvider };
+export * from './hooks/common/types';
+export * from './hooks/use-cart/use-cart.types';
+export * from './hooks/use-checkout/use-checkout.types';

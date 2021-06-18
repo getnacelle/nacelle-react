b48bad2747be79f734e925124beef31dfac40ee3
Nick Richmond <nick@getnacelle.com>
2021-06-18 13:58:23 -0400

    use new convention for public env vars

--- examples/nextjs/services/nacelleClient.js
+++ examples/nextjs/services/nacelleClient.js
@@ -3,8 +3,8 @@ import NacelleClient from '@nacelle/client-js-sdk';
 export default new NacelleClient({
   // for a full list of configuration options, please see
   // https://docs.getnacelle.com/api-reference/client-js-sdk.html
-  id: process.env.NACELLE_SPACE_ID,
-  token: process.env.NACELLE_GRAPHQL_TOKEN,
-  nacelleEndpoint: process.env.NACELLE_ENDPOINT,
+  id: process.env.NEXT_PUBLIC_NACELLE_SPACE_ID,
+  token: process.env.NEXT_PUBLIC_NACELLE_GRAPHQL_TOKEN,
+  nacelleEndpoint: process.env.NEXT_PUBLIC_NACELLE_ENDPOINT,
   useStatic: false
 });

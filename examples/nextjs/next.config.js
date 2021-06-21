f80ac13fb662cd44114b711dd6d3eab3f34fb2d5
Nick Richmond <nick@getnacelle.com>
2021-06-21 10:18:11 -0400

    use new convention for public env vars

--- examples/nextjs/next.config.js
+++ examples/nextjs/next.config.js
@@ -1,9 +1,4 @@
 module.exports = {
-  env: {
-    NACELLE_SPACE_ID: process.env.NACELLE_SPACE_ID,
-    NACELLE_GRAPHQL_TOKEN: process.env.NACELLE_GRAPHQL_TOKEN,
-    NACELLE_ENDPOINT: process.env.NACELLE_ENDPOINT
-  },
   images: {
     domains: [
       'cdn.shopify.com',

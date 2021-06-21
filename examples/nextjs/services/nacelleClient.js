931fa79657327f654032c621359bb5991100e3e6
Nick Richmond <nick@getnacelle.com>
2021-06-21 14:56:25 -0400

    check previewData for keys of interest

--- examples/nextjs/services/nacelleClient.js
+++ examples/nextjs/services/nacelleClient.js
@@ -13,68 +13,73 @@ const nacelleClientOptions = {
 const defaultClient = new NacelleClient(nacelleClientOptions);
 const unifiedClient = new NacelleClient(nacelleClientOptions);
 
+const hasCmsEnvVars = (params) =>
+  params?.contentfulSpaceId && params?.contentfulPreviewApiToken;
+const hasPimEnvVars = (params) =>
+  params?.myShopifyDomain && params?.shopifyStorefrontAccessToken;
+
 unifiedClient.data.content = (params) =>
-  params.previewData
+  hasCmsEnvVars(params.previewData)
     ? createContentfulClient(params.previewData).content(params)
     : defaultClient.data.content(params);
 
 unifiedClient.data.page = (params) =>
-  params.previewData
+  hasCmsEnvVars(params.previewData)
     ? createContentfulClient(params.previewData).page(params)
     : defaultClient.data.page(params);
 
 unifiedClient.data.pages = (params) =>
-  params.previewData
+  hasCmsEnvVars(params.previewData)
     ? createContentfulClient(params.previewData).pages(params)
     : defaultClient.data.pages(params);
 
 unifiedClient.data.article = (params) =>
-  params.previewData
+  hasCmsEnvVars(params.previewData)
     ? createContentfulClient(params.previewData).article(params)
     : defaultClient.data.article(params);
 
 unifiedClient.data.articles = (params) =>
-  params.previewData
+  hasCmsEnvVars(params.previewData)
     ? createContentfulClient(params.previewData).articles(params)
     : defaultClient.data.articles(params);
 
 unifiedClient.data.blog = (params) =>
-  params.previewData
+  hasCmsEnvVars(params.previewData)
     ? createContentfulClient(params.previewData).blog(params)
     : defaultClient.data.blog(params);
 
 unifiedClient.data.allContent = (params) =>
-  params?.previewData
+  hasCmsEnvVars(params?.previewData)
     ? createContentfulClient(params.previewData).allContent(params)
     : defaultClient.data.allContent(params);
 
 unifiedClient.data.product = (params) =>
-  params.previewData
+  hasPimEnvVars(params.previewData)
     ? createShopifyClient(params.previewData).product(params)
     : defaultClient.data.product(params);
 
 unifiedClient.data.products = (params) =>
-  params.previewData
+  hasPimEnvVars(params.previewData)
     ? createShopifyClient(params.previewData).products(params)
     : defaultClient.data.products(params);
 
 unifiedClient.data.collection = (params) =>
-  params.previewData
+  hasPimEnvVars(params.previewData)
     ? createShopifyClient(params.previewData).collection(params)
     : defaultClient.data.collection(params);
 
 unifiedClient.data.collectionPage = (params) =>
-  params.previewData
+  hasPimEnvVars(params.previewData)
     ? createShopifyClient(params.previewData).collectionPage(params)
     : defaultClient.data.collectionPage(params);
 
 unifiedClient.data.allProducts = (params) =>
-  params?.previewData
+  hasPimEnvVars(params?.previewData)
     ? createShopifyClient(params.previewData).allProducts(params)
     : defaultClient.data.allProducts(params);
 
 unifiedClient.data.allCollections = (params) =>
-  params?.previewData
+  hasPimEnvVars(params?.previewData)
     ? createShopifyClient(params.previewData).allCollections(params)
     : defaultClient.data.allCollections(params);
 

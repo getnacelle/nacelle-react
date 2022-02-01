252d80914d786de04911da9e1148ef5dfad6aa69
Jason Redick <jason.redick@me.com>
2022-02-01 11:41:20 -0800

    chore: remove gatsby-source-nacelle

--- packages/gatsby-source-nacelle/src/utils/index.js
+++
@@ -1,5 +0,0 @@
-exports.cacheIsInvalid = require('./cacheIsInvalid');
-exports.cmsPreviewEnabled = require('./cmsPreviewEnabled');
-exports.createRemoteImageFileNode = require('./createRemoteImageFileNode');
-exports.hasBeenIndexedSinceLastBuild = require('./checkIndexTimestamp');
-exports.replaceKey = require('./replaceKey');

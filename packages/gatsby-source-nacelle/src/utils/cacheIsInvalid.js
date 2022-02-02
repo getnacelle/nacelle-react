cb01428027db7a0ca154aa256941fbd760edac77
Jason Redick <jason.redick@me.com>
2022-02-02 15:15:23 -0800

    chore: remove gatsby-source-nacelle (#79)

--- packages/gatsby-source-nacelle/src/utils/cacheIsInvalid.js
+++
@@ -1,6 +0,0 @@
-module.exports = function (timestamp, pluginOptions) {
-  return (
-    Date.now() - timestamp >
-    (pluginOptions.cacheDuration || 24 * 60 * 60 * 1000)
-  );
-};
--- packages/gatsby-source-nacelle/src/utils/checkIndexTimestamp.js
+++
@@ -1,7 +0,0 @@
-module.exports = function (itemInIndex, lastFetched) {
-  return (
-    lastFetched === undefined ||
-    (lastFetched !== undefined &&
-      itemInIndex.indexedAt * 1000 - lastFetched > 0)
-  );
-};
